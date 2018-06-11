48d6aff92a40d2f7a848a9a1a76420f6414734a7
Kevin Lim <38468417+kevslim@users.noreply.github.com>
2018-06-11 17:46:42 -0500

    cleaning up some files

--- bubblesort.js
+++ bubblesort.js
@@ -1,26 +1,24 @@
 function bubbleSort(array) {
-	let swapped;
-	do {
-		console.log("inside do");
-		swapped = false;
-		for(let i = 0; i < array.length - 1; i++) {
-			if(array[i] > array[i+1]) {
-				swap(array, i, i+1);
-				console.log(array[i], array[i+1]);
-				swapped = true;
+	// let swapped;
+	// do {
+	// 	swapped = false;
+	// 	for(let i = 0; i < array.length - 1; i++) {
+	// 		if(array[i] > array[i+1]) {
+	// 			swap(array, i, i+1);
+	// 			swapped = true;
+	// 		}
+	// 	}
+	// } while(swapped);
+	// return array;
+
+	for(let end = array.length - 1; end > 0; end--) {
+		for(let j = 0; j < end; j++) {
+			if(array[j] > array[j+1]) {
+				swap(array, j, j+1);
 			}
 		}
-	} while(swapped);
+	}
 	return array;
-
-// 	// for(let end = array.length - 1; end > 0; end--) {
-// 	// 	for(let j = 0; j < end; j++) {
-// 	// 		if(array[j] > array[j+1]) {
-// 	// 			swap(array[i], array[i+1]);
-// 	// 		}
-// 	// 	}
-	// }
-	// return array;
 }
 
 function swap(arr, a, b) {
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -32,8 +32,6 @@ describe('mergeSort', function() {
     spyOn(window, 'split').and.callThrough();
   });
 
-
-
   it('sorts an array into 1 sorted array', function() {
     expect(mergeSort([6, 5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5, 6]);
     expect(mergeSort([2, 4, 6, 1, 3, 5])).toEqual([1, 2, 3, 4, 5, 6]);
